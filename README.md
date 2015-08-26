c691f5a94266affbc75fe46dbe429cc4072aebc8
Timothy Tong <tktong@ucdavis.edu>
2015-08-25 21:11:14 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Spring Boot MVC Sandbox
+
+A minimum standalone Spring Boot MVC with a single controller, service, repository, and entity. Useful for isolated prototyping.
+
+## Gradle
+
+- `gradle build`
+- `gradle bootRun`
---
+++ src/main/java/org/tktong/SandboxController.java
@@ -0,0 +1,17 @@
+package org.tktong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SandboxController {
+	@Autowired
+	private SandboxService sandboxService;
+
+	@RequestMapping(value = "/*")
+	public String sandbox(Model model) {
+		return "index";
+	}
+}
